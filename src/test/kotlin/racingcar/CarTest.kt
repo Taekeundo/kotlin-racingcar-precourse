b42649838966f854a3e4bbc1c2847e35cad71f59
Taekeundo <pleasepose@gmail.com>
2025-05-07 14:00:48 +0200

    test(car): add unit test for carMove()
    - Add CarTest to verify that carMove() forwart when move() is called
    - Ensured that position is incremented correctly

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,20 @@
+package racingcar
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+/**
+ *  [ Test index ]
+ *  1. car.move         Basic(valid)
+ */
+class CarTest {
+
+    @Test
+    fun `Car moves forward when move() is called`() {
+        val car = Car("pobi")
+        car.move()
+        car.move()
+
+        assertThat(car.position).isEqualTo(2)
+    }
+}
