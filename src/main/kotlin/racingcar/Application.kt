738ca9601ceeaad971dd7b4f5122852a56ae7da1
Taekeundo <pleasepose@gmail.com>
2025-05-07 14:41:50 +0200

    refactor(view): extract OutputView from App.kt
    - Move result and message output logic from main to OutputView
    - It handles car positions, race headers, winners, and ask messages
    - Simplefied main by delegating all output responsibilities

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,12 +7,12 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
 
     /* (1) Input(1) carNames */
-    println("Enter the names of the cars (comma-separated):")
+    OutputView.askCarNames()
     val carNames = InputView.readCarNames()
     validateCarNames(carNames)
 
     /* (2) Input(2) nMoves */
-    println("How many rounds will be played?")
+    OutputView.askNumberOfMoves()
     val nMovesInput = InputView.readNumberOfMoves()
     validateNumberOfMoves(nMovesInput)
     val nMoves = nMovesInput.toInt()
@@ -24,27 +24,23 @@ fun main() {
     val carList = carNames.map { Car(it) }
 
     /* (4) Start Racing */
-    println("Race Results")
+    OutputView.printRaceResultsHeader()
     repeat(nMoves) {
         carList.forEach { car ->
             val randomNumber = Randoms.pickNumberInRange(0, 9)
             if (randomNumber >= 4)
                 car.move()
         }
-        carList.forEach { car ->
-            println("${car.name} : ${"-".repeat(car.position)}")
-        }
-        println()
+        OutputView.printCarPositions(carList.map {it.name to it.position})
     }
 
     /* (5) Find winner */
     val maxPosition: Int = carList.maxOf { it.position }
-
     val winners: List<String> = carList
         .filter { it.position == maxPosition }
         .map { it.name }
 
-    println("Winners : ${winners.joinToString(", ")}")
+    OutputView.printWinners(winners)
 }
 
 fun validateCarNames(carNames: List<String>) {
--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,5 +1,25 @@
 package racingcar.view
 
 object OutputView {
+    fun askCarNames() {
+        println("Enter the names of the cars (comma-separated):")
+    }
 
+    fun askNumberOfMoves() {
+        println("How many rounds will be played?")
+    }
+
+    fun printRaceResultsHeader() {
+        println("Race Results")
+    }
+
+    fun printCarPositions(carStatus: List<Pair<String, Int>>) {
+        val result = carStatus.map { (name, position) -> "$name: ${"-".repeat(position)}" }
+        result.forEach { println(it) }
+        println()
+    }
+
+    fun printWinners(winners: List<String>) {
+        println("Winners : ${winners.joinToString(", ")}")
+    }
 }
