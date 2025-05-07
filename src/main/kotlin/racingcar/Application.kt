7565f7a4a088e6faaa7cfe2f678654bd6d394c77
Taekeundo <pleasepose@gmail.com>
2025-05-07 12:28:47 +0200

    feat(input): implement car name input and validataion
    - Add input logic for carNames with comma-separated parsing and trimming
    - Implemente validation: empty, over 5 chars -> Not allowed
    - Throws IllegalArgumentException when invalid input comes

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,26 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val carLists = Console.readLine()
+        .split(",")
+        .map { it.trim() }
+
+    validateCarNames(carLists)
+
+    println("Valid car names: $carLists")
 }
+
+fun validateCarNames(carNames: List<String>) {
+    carNames.forEach { name ->
+        if ((name.isEmpty())) {
+            throw IllegalArgumentException("Car name cannot be empty.")
+        }
+        if (name.length > 5) {
+            throw IllegalArgumentException("Car name must be 5 characters or less.")
+        }
+    }
+}
+
