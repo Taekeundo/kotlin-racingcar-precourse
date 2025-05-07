a203be6dad70fb5bd849c09551b5ab51bdec6963
Taekeundo <pleasepose@gmail.com>
2025-05-07 12:40:01 +0200

    feat(input): implement nMoves input and validation
    - Add input logic for nRounds to be played
    - Implement validation to ensure input is int and greater than 0

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,14 +3,24 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    // Input(1) carNames
     println("Enter the names of the cars (comma-separated):")
     val carLists = Console.readLine()
         .split(",")
         .map { it.trim() }
 
     validateCarNames(carLists)
-
     println("Valid car names: $carLists")
+
+    // Input(2) nMoves
+    println("How many rounds will be played?")
+    val nMovesInput = Console.readLine()
+
+    validateNumberOfMoves(nMovesInput)
+
+    val nMoves = nMovesInput.toInt()
+
+    println("Valid number of moves: $nMoves")
 }
 
 fun validateCarNames(carNames: List<String>) {
@@ -24,3 +34,11 @@ fun validateCarNames(carNames: List<String>) {
     }
 }
 
+fun validateNumberOfMoves(input: String) {
+    val number = input.toIntOrNull()
+        ?: throw IllegalArgumentException("Number of moves must be a valid integer.")
+
+    if (number <= 0) {
+        throw IllegalArgumentException("Number of moves must be greater than 0.")
+    }
+}
